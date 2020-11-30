a65801c151547108eebd4dfb77c1324c684aebf2
何文垚 <hwy205441376@163.com>
2020-11-30 11:07:25 +0800

    modify redis config file

--- spring-boot-redis/src/main/java/com/example/springbootredis/config/RedisConfig.java
+++ spring-boot-redis/src/main/java/com/example/springbootredis/config/RedisConfig.java
@@ -1,44 +1,30 @@
 package com.example.springbootredis.config;
 
-import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.io.Serializable;
 
 @Configuration
 public class RedisConfig {
 
-    /**
-     * 默认情况下RedisTemplate模板只能支持字符串，我们自定义一个RedisTemplate，
-     * 设置序列化器，这样我们可以很方便的操作实例对象。
-     * @param connectionFactory
-     * @return
-     */
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(
-            LettuceConnectionFactory connectionFactory){
-        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
-        ObjectMapper om = new ObjectMapper();
-        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-        jackson2JsonRedisSerializer.setObjectMapper(om);
-        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
-        template.setConnectionFactory(connectionFactory);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(jackson2JsonRedisSerializer);
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(jackson2JsonRedisSerializer);
-        template.setDefaultSerializer(new StringRedisSerializer());
-        template.afterPropertiesSet();
-        return template;
+    public RedisTemplate<?,?> initRedisTemplate(RedisConnectionFactory redisConnectionFactory ){
+        RedisTemplate<?,?> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        // key采用String序列化方式
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        // 初始化一个FastJSON解析器
+        FastJsonRedisSerializer<?> fastJsonRedisSerializer = new FastJsonRedisSerializer<>(Object.class);
+        //value采用fast-json序列化方式。
+        redisTemplate.setValueSerializer(fastJsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(fastJsonRedisSerializer);
+        redisTemplate.setDefaultSerializer(stringRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
     }
+
 }
--- spring-boot-redis/src/test/java/com/example/springbootredis/SpringBootRedisApplicationTests.java
+++ spring-boot-redis/src/test/java/com/example/springbootredis/SpringBootRedisApplicationTests.java
@@ -1,11 +1,9 @@
 package com.example.springbootredis;
 
-import com.example.springbootredis.entity.UserEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 
 @SpringBootTest
 class SpringBootRedisApplicationTests {
@@ -15,13 +13,8 @@ class SpringBootRedisApplicationTests {
 
     @Test
     void contextLoads() {
-        UserEntity entity = new UserEntity(1,"何文垚","男");
-        ValueOperations<String,UserEntity> operations = redisTemplate.opsForValue();
-        operations.set("1",entity);
-        boolean exits  = redisTemplate.hasKey("1");
-        System.out.println("是否存在"+exits);
-        UserEntity entity1 = (UserEntity) redisTemplate.opsForValue().get("1");
-
+        String name = (String) redisTemplate.opsForValue().get("name");
+        System.out.println(name);
     }
 
 }
